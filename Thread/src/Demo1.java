25faa3d1e8841bbb5454447e1d3717020d84bdc9
liym <15079447645@163.com>
2021-03-09 16:51:43 +0800

    demo commit

---
+++ Thread/src/Demo1.java
@@ -0,0 +1,8 @@
+// 进程 = 一个应用程序
+// 线程 = 一个请求。一个执行路径
+// 一个进程 = N个线程
+public class Demo1 {
+    public static void main(String[] args){
+        System.out.println("1111111111");
+    }
+}
---
+++ Thread/src/Demo2.java
@@ -0,0 +1,22 @@
+// 继承 Thread
+public class Demo2{
+    public static void main(String[] args){
+        // 创建一个新线程
+       createThread ct = new createThread();
+       ct.start(); // start 启用线程。 执行run方法
+        for (int i = 0; i < 20; i++){
+            System.out.println("man() i:" + i);
+        }
+    }
+}
+
+
+class createThread extends Thread {
+    // 执行需要线程执行的任务 代码
+    @Override
+    public void run() {
+        for (int i = 0; i < 20; i++){
+            System.out.println("run() i:" + i);
+        }
+    }
+}
